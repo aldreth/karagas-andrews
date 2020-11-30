cd26c3cbb6425039a9607ff99612d6c87c632634
Edward Andrews-Hodgson <edward@aldreth.com>
2020-11-30 01:15:21 +0000

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,11 @@ module.exports = {
   },
   variants: {},
   plugins: [],
-  purge: ["./src/**/*.html", "./public/**/*.html"],
+  // purge: ["./src/**/*.html", "./public/**/*.html"],
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: ["./public/**/*.html"],
+    mode: "all",
+    preserveHtmlElements: false,
+  },
 };
